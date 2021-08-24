d5a4dcac7da7ef2fc4fdc39003ad2332a393cf03
Orozcastro <47483805+Orozcastro@users.noreply.github.com>
2021-08-24 16:14:25 -0500

    Se agrego la bd al query de register

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.post('/auth', async (req,res) => {
     const pass = req.body.pass;
     let passwordHaash = await bcryptjs.hash(pass, 8);
     if(user && pass){
-        connection.query('SELECT * FROM users WHERE user =?', [user], async (error, results)=>{
+        connection.query('SELECT * FROM login.users WHERE user =?', [user], async (error, results)=>{
             if(results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))){
                 res.render("login", {
                   alert: true,
